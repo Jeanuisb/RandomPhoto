6c1d9c6c7fe5a1fe4130ec5bceaa65f08e78f39d
Jean Bikorimana <94160288+Jeanuisb@users.noreply.github.com>
2023-03-29 17:08:07 -0500

    creating new gradient layer BUG: 	image is not showing. I'm assuming that the image is behind the background layer

--- Random photo/ViewController.swift
+++ Random photo/ViewController.swift
@@ -8,27 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.backgroundColor = .white
         return imageView
     }()
-    
-   
-        func load(url: URL) {
-            DispatchQueue.global().async { [weak self] in
-                if let data = try? Data(contentsOf: url) {
-                    if let image = UIImage(data: data) {
-                        DispatchQueue.main.async {
-                            self?.image = image
-                        }
-                    }
-            }
-        }
-    }
-    
     
     private let button: UIButton = {
         let button = UIButton()
@@ -47,15 +33,13 @@ class ViewController: UIViewController {
         .systemOrange,
         .systemPurple
         
-    
+        
     ]
-    
-    let url = "https://source.unsplash.com/random/600x600"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        
         
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0,
@@ -66,19 +50,24 @@ class ViewController: UIViewController {
         
         
         
-        //getRandomPhoto()
+        gradientBackground()
+        getRandomPhoto()
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+        
+        
+        
     }
     
     @objc func didTapButton(){//checks if the user tapped the button.
         
-        //getRandomPhoto()
+        getRandomPhoto()
         view.backgroundColor = colors.randomElement()
     }
     
     override func viewDidLayoutSubviews() {
         
-            super.viewDidLayoutSubviews()
+        super.viewDidLayoutSubviews()
         
         view.addSubview(button)
         button.frame = CGRect( //rearranges button so that we don't have it above the home bar
@@ -91,48 +80,18 @@ class ViewController: UIViewController {
         
     }//"https://source.unsplash.com/random/600x600
     
-    /*
-    func getRandomPhoto() { //grabs url for the image
+    func getRandomPhoto() {
         let urlString = "https://source.unsplash.com/random/600x600"
         let url = URL(string:urlString)!
         
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                if let error = error {
-                    self.handleClientError(error)
-                    return
-                }
-                guard let httpResponse = response as? HTTPURLResponse,
-                    (200...299).contains(httpResponse.statusCode) else {
-                    self.handleServerError(response)
-                    return
-                }
-                if let mimeType = httpResponse.mimeType, mimeType == "text/html",
-                    let data = data,
-                    let string = String(data: data, encoding: .utf8) {
-                    DispatchQueue.main.async {
-                        self.webView.loadHTMLString(string, baseURL: url)
-                    }
-                }
-            }
-            task.resume()
+        guard let data = try? Data(contentsOf: url) else{
+            return
+        }
         
-    } */
-    
-    
-    /*func setImageFromStringrURL(stringUrl: String) {
-        if let url = URL(string:"https://source.unsplash.com/random/600x600") {
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-          // Error handling...
-          guard let imageData = data else { return }
-
-          DispatchQueue.main.async {
-            self.image = UIImage(data: imageData)
-              
-          }
-        }.resume()
-      }
+        imageView.image = UIImage(data: data)
+        
     }
-     */
+    
     
     
     
@@ -141,19 +100,21 @@ class ViewController: UIViewController {
         let gradient = CAGradientLayer()
         
         gradient.colors = [UIColor(red: 0.91, green: 0.78, blue: 0.47, alpha: 1.00).cgColor,
-                                UIColor(red: 0.25, green: 0.61, blue: 0.52, alpha: 1.00).cgColor,
-                                UIColor(red: 0.72, green: 0.62, blue: 0.71, alpha: 1.00).cgColor
+                           UIColor(red: 0.25, green: 0.61, blue: 0.52, alpha: 1.00).cgColor,
+                           UIColor(red: 0.72, green: 0.62, blue: 0.71, alpha: 1.00).cgColor
                            
         ]
         
         view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
         
         
     }
     
     
-   
-
-
+    
+    
 }
+
+
 
